0662f6c9b292efd13e474b6f4e63509aba453bd6
Teja_Nethas <saitejau12@gmail.com>
2020-12-18 14:55:41 +0530

    update ts file
    update ts file

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  public memberBillingTrainingForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit() {
+
+    this.memberBillingTrainingForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: [ '', Validators.required],
+      emailAddress: ['', Validators.required],
+      clinicFacilityName: ['', Validators.required],
+      TIN: [''],
+      completedTheProgram: ['', Validators.required],
+      didYouUnderstand: ['', Validators.required],
+      question1: ['', Validators.required],
+      question2: ['', Validators.required],
+      question3: ['', Validators.required],
+      comments: ['', Validators.required],
+    });
+
+
+  }
+
+  get f() { return this.memberBillingTrainingForm.controls; }
+
+  onSubmit(){
+    console.log(this.memberBillingTrainingForm.value);
   }
 
 }
